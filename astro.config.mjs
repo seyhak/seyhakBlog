bd616a62a1e060abbdc66ef9b69a6ca76db7b987
seyhak <seyhak.ly@gmail.com>
2024-02-05 00:14:03 +0100

    v1.2.0
    * feat: Contact form
    
    * feat: new pics and co
    
    * feat: animated doesnt disappear
    
    * feat: carousel and contact form
    
    * fix: animations
    
    * feat: disable contact turn off, fix script
    
    ---------
    
    Co-authored-by: Seyhak <seyhakly@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config"
-
 import tailwind from "@astrojs/tailwind"
+
+import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +11,5 @@ export default defineConfig({
   // },
   // https://docs.astro.build/en/recipes/i18n/
   projectRoot: "./src",
-  integrations: [tailwind()]
+  integrations: [tailwind(), react()]
 })
---
+++ src/components/react/Button.tsx
@@ -0,0 +1,18 @@
+import classNames from "classnames"
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>
+
+export const Button = ({ children, className, ...props }: ButtonProps) => {
+  return (
+    <button
+      {...props}
+      className={classNames([
+        "Button px-3 py-1 rounded bg-white/20",
+        "hover:shadow-lg hover:shadow-pink-500 dark:hover:shadow-pink-800",
+        "transition-shadow duration-500",
+        className
+      ])}
+    >
+      {children}
+    </button>
+  )
+}
